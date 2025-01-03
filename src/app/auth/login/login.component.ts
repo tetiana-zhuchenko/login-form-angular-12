43800670ee72a7b2e5601ee884fd27f20ea6aafb
Tania Zhuchenko <taniyhamail@gmail.com>
2025-01-03 13:17:15 +0200

    feat: added form validation status

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -12,6 +12,11 @@ export class LoginComponent {
   onSubmit(formData: NgForm) {
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
+    if (formData.form.invalid) {
+      return;
+    }
+
+    console.log(formData.form);
     console.log('formData', enteredEmail, enteredPassword);
   }
 }
